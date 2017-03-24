d849b55782a14f117ac42a15919ac3c1180018b0
liudaye <liudayedian@163.com>
2017-03-24 10:34:45 +0800

    init

---
+++ src/test/java/com/liudaye/CollectionsTest.java
@@ -0,0 +1,15 @@
+package com.liudaye;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Liuhongming
+ * @version 1.0
+ * @date 2017/3/24 10:03
+ * @Description: 一句话简单说明功能。。
+ */
+public class CollectionsTest {
+    private final static Logger log = LoggerFactory.getLogger(CollectionsTest.class);
+    
+}
---
+++ src/test/java/com/liudaye/collections/ArrayListTest.java
@@ -0,0 +1,18 @@
+package com.liudaye.collections;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Liuhongming
+ * @version 1.0
+ * @date 2017/3/24 10:23
+ * @Description: array list test
+ */
+public class ArrayListTest {
+    private final static Logger log = LoggerFactory.getLogger(ArrayListTest.class);
+    
+    public static void main(String[] args) {
+        
+    }
+}
