4d60a73f86801a09dd85fbe3533a90392a7ef71d
liudaye <liudayedian@163.com>
2017-03-24 11:23:47 +0800

    init

---
+++ src/test/java/com/liudaye/collections/LinkedTest.java
@@ -0,0 +1,18 @@
+package com.liudaye.collections;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Liuhongming
+ * @version 1.0
+ * @date 2017/3/24 11:22
+ * @Description: 一句话简单说明功能。。
+ */
+public class LinkedTest {
+    private final static Logger log = LoggerFactory.getLogger(LinkedTest.class);
+    
+    public static void main(String[] args) {
+        
+    }
+}
